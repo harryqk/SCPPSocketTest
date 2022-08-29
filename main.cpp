1c73bf7562589915e4a06939cd0782909076480d
dragonqk <dragonqk@163.com>
2022-08-29 10:38:03 +0800

    add protobuf test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <library.h>
+
 #include <sstream>
 #include "NetManager.h"
+#include "Harry1.pb.h"
 using namespace scppsocket;
 using namespace std;
+using namespace tutorial;
 
-void WaitForCommand();
 void OnServerMessageRead(int fd, const char* msg, int len);
 void OnClientMessageRead(const char* msg, int len);
-
+NetManager Mgr;
 void OnClientMessageRead(const char *msg, int len)
 {
     char* str = new char[len + 1];
@@ -30,6 +31,49 @@ void OnServerMessageRead(int fd, const char *msg, int len)
     delete[] str;
 }
 
+void SendProtobufMessage()
+{
+    Harry1 example1;
+    example1.set_stringval("hello,world");
+    example1.set_bytesval("are you ok?");
+
+    Harry1_EmbeddedMessage* embeddedExample2 = new Harry1_EmbeddedMessage();
+
+    embeddedExample2->set_int32val(1);
+    embeddedExample2->set_stringval("embeddedInfo");
+    example1.set_allocated_embeddedexample1(embeddedExample2);
+
+    example1.add_repeatedint32val(2);
+    example1.add_repeatedint32val(3);
+    example1.add_repeatedstringval("repeated1");
+    example1.add_repeatedstringval("repeated2");
+    string str;
+    example1.SerializeToString(&str);
+    Harry1 example2;
+    example2.ParseFromString(str);
+
+    printf("1:%s\n", example2.stringval().c_str());
+    printf("3:%d\n", example2.embeddedexample1().int32val());
+    printf("4:%s\n", example2.embeddedexample1().stringval().c_str());
+
+    for (int i = 0; i < example2.repeatedint32val().size(); i++)
+    {
+        printf("5:%d\n", example2.repeatedint32val(i));
+    }
+
+    for (int i = 0; i < example2.repeatedstringval().size(); i++)
+    {
+        printf("6:%s\n", example2.repeatedstringval(i).c_str());
+    }
+    int BUFFSIZE = 1024;
+    char buff[BUFFSIZE];
+    example2.SerializeToArray(buff,BUFFSIZE);
+
+
+    Mgr.TCPClientSendMessage(buff, strlen(buff));
+
+}
+
 
 std::vector<std::string> StringSplit(const std::string& str, char delim) {
     std::stringstream ss(str);
@@ -45,7 +89,6 @@ std::vector<std::string> StringSplit(const std::string& str, char delim) {
 
 int main()
 {
-    NetManager Mgr;
     const int size = 50;
     char buf[size];
     printf("please input command\n");
@@ -69,6 +112,8 @@ int main()
                 Mgr.StartTCPClient("192.168.1.104", 1500);
 #elif __APPLE__
                 Mgr.StartTCPClient("192.168.1.9", 1500);
+                //Mgr.StartTCPClient("192.168.1.102", 1500);
+                //Mgr.StartTCPClient("192.168.1.101", 1500);
 #elif __linux__
                 Mgr.StartTCPClient("10.0.2.15", 1500);
 #endif
@@ -105,7 +150,8 @@ int main()
                     std::cout << "send message is:" << array[1] << std::endl;
                     if(mode == 0)
                     {
-                        Mgr.TCPClientSendMessage(array[1].c_str(), array[1].length());
+                        SendProtobufMessage();
+                        //Mgr.TCPClientSendMessage(array[1].c_str(), array[1].length());
                     }
                     else
                     {
